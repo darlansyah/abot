73135360e9cab9fb5fa88710bb6e5d8eb6b3df53
abdmalik23 <abdulmalik2397@gmail.com>
2019-07-26 00:23:02 +0700

    update file file hasil.php

--- juri/header.php
+++ juri/header.php
@@ -84,7 +84,7 @@ desired effect
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Juri</b>LTE</span>
+      <span class="logo-lg"><b><?php echo   $_SESSION['nama'] ?></b></span>
     </a>
 
     <!-- Header Navbar -->
--- juri/index.php
+++ juri/index.php
@@ -1,12 +1,4 @@
-<?php
-include('header.php');
-
-
-
- ?>
-
-
-
+<?php include('header.php');?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
