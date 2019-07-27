4ee952af962a9a0fd7571070225d0e9b3c4df93f
darlansyah <darlansyah96@gmail.com>
2019-07-27 17:49:39 +0700

    admin edit upload lomba

--- admin/lomba_edit.php
+++ admin/lomba_edit.php
@@ -35,12 +35,20 @@ if($data['status_lomba'] == 'Posting') {
             <div class="col-md-12">
                 <!-- Horizontal Form -->
                 <div class="box box-info">
-                    
+
                     <!-- /.box-header -->
                     <!-- form start -->
                     <form method="post" action="lomba_update.php" method="post" enctype="multipart/form-data">
+                      <input type="hidden" name="id" value="<?= $data['id_lomba'] ?>">
+                      <input type="hidden" name="foto_lama" value="<?= $data['foto'] ?>">
                         <div class="box-body">
-                            <div class="form-group col-md-10">
+                          <div class="form-group col-md-12">
+
+                              <img width="100%"  src="upload/<?= $data['foto'] ?>" alt="foto"> <br/> <br/>
+
+                          </div>
+
+                            <div class="form-group col-md-12">
                                 <label>Judul</label>
                                 <input type="text" class="form-control" placeholder="Judul Lomba" name="judul" value="<?php echo $data['judul'];?>">
 
@@ -51,7 +59,7 @@ if($data['status_lomba'] == 'Posting') {
                                     <div class="input-group-addon">
                                         <i class="fa fa-calendar"></i>
                                     </div>
-                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_daftar" value="<?php echo date("d-m-Y", strtotime($data['tanggal_daftar']));?>">
+                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_daftar" value="<?php echo date("d-m-Y", strtotime($data['tgl_daftar']));?>">
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
@@ -60,7 +68,7 @@ if($data['status_lomba'] == 'Posting') {
                                     <div class="input-group-addon">
                                         <i class="fa fa-calendar"></i>
                                     </div>
-                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_mulai" value="<?php echo date("d-m-Y", strtotime($data['tanggal_mulai']));?>">
+                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_mulai" value="<?php echo date("d-m-Y", strtotime($data['tgl_mulai']));?>">
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
@@ -69,35 +77,34 @@ if($data['status_lomba'] == 'Posting') {
                                     <div class="input-group-addon">
                                         <i class="fa fa-calendar"></i>
                                     </div>
-                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_selesai" value="<?php echo date("d-m-Y", strtotime($data['tanggal_selesai']));?>">
+                                    <input type="text" class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask name="tanggal_selesai" value="<?php echo date("d-m-Y", strtotime($data['tgl_selesai']));?>">
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
                                 <label>Biaya Daftar</label>
-                                <input type="text" class="form-control" placeholder="biaya daftar" name="biaya_daftar" value="<?php echo $data['biaya_daftar'];?>">
+                                <input type="text" class="form-control" placeholder="biaya daftar" name="biaya_daftar" value="<?php echo $data['biaya_lomba'];?>">
                             </div>
                             <div class="form-group col-md-4">
                                 <label>Foto</label>
-                                <input type="file" name="foto">
+                                  <input  type="file" name="foto">
                             </div>
                             <div class="form-group col-md-4">
-                                <label>
+                                <label> Status </label> <br/>
                                     <input type="radio" name="status_lomba" class="minimal" value="Draft" <?php echo $draft;?>> draft
-                                </label>
-                                <label>
                                     <input type="radio" name="status_lomba" class="minimal" value="Posting" <?php echo $posting;?>> Posting
-                                </label>
+
                             </div>
                             <div class="form-group col-md-12">
                                 <label>Deskripsi</label>
-                                <textarea class="textarea" name="deskripsi" value="<?php echo $data['judul'];?>" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" >
-                                              
+                                <textarea class="textarea" name="deskripsi" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;" >
+                                  <?php echo $data['deskripsi'];?>
                                 </textarea>
                             </div>
                         </div>
                         <div class="box-footer">
                             <button type="submit" class="btn btn-primary">Update</button>
                         </div>
+
                         <!-- /.box-footer -->
                     </form>
                 </div>
